7986d17c4952f8e17a977ad261f891b55989d870
Taishawnk <Taishawnking1@yahoo.com>
2020-09-07 02:36:12 -0600

    added-one-to-many-assosiation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ before_action :find_article_id, only: [:show, :edit, :update, :destroy]
 
    def create
      @article = Article.new(article_params)
+     @article.user = User.first
      if @article.save
       flash[:notice] = "Article was created sucseefully."
      redirect_to @article
