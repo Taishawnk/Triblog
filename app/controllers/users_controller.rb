6e69f3e7a4e8c2fb1515170a08c1dc3a4ea16175
Taishawnk <Taishawnking1@yahoo.com>
2020-09-10 11:32:17 -0600

    set up signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def create
+    @user = User.new(user_params )
+    if @user.save 
+        flash[:notice] = "Welcome to TriBlog #{@user.username}, your account has successfully been created!"
+        redirect_to articles_path
+    else 
+        render 'new'
+         end 
+    end
+
+    private 
+
+    def user_params 
+         params.require(:user).permit(:username, :email, :password)
+    end 
+
 end
